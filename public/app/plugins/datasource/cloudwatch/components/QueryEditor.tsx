aa729fe6ae579e62d7d6aee28be42cf005a7cdeb
Erik Sundell <erik.sundell87@gmail.com>
2019-11-28 22:44:31 +0100

    Break out non annontations specific fields

--- public/app/plugins/datasource/cloudwatch/components/QueryEditor.tsx
+++ public/app/plugins/datasource/cloudwatch/components/QueryEditor.tsx
@@ -1,15 +1,67 @@
-import React, { PureComponent } from 'react';
+import React, { PureComponent, ChangeEvent } from 'react';
 import { QueryEditorProps } from '@grafana/data';
+import { Input, ValidationEvents, EventsWithValidation, Switch } from '@grafana/ui';
 import { CloudWatchQuery } from '../types';
 import CloudWatchDatasource from '../datasource';
-import { QueryFieldsEditor } from './QueryFieldsEditor';
+import { QueryField, Alias, QueryFieldsEditor } from './';
 
 export type Props = QueryEditorProps<CloudWatchDatasource, CloudWatchQuery>;
 
-interface State {}
+interface State {
+  showMeta: boolean;
+}
+
+const idValidationEvents: ValidationEvents = {
+  [EventsWithValidation.onBlur]: [
+    {
+      rule: value => new RegExp(/^$|^[a-z][a-zA-Z0-9_]*$/).test(value),
+      errorMessage: 'Invalid format. Only alphanumeric characters and underscores are allowed',
+    },
+  ],
+};
 
 export class QueryEditor extends PureComponent<Props, State> {
-  state: State = {};
+  state: State = { showMeta: false };
+
+  componentWillMount() {
+    const { query } = this.props;
+
+    if (!query.namespace) {
+      query.namespace = '';
+    }
+
+    if (!query.metricName) {
+      query.metricName = '';
+    }
+
+    if (!query.expression) {
+      query.expression = '';
+    }
+
+    if (!query.dimensions) {
+      query.dimensions = {};
+    }
+
+    if (!query.region) {
+      query.region = 'default';
+    }
+
+    if (!query.id) {
+      query.id = '';
+    }
+
+    if (!query.alias) {
+      query.alias = '';
+    }
+
+    if (!query.statistics || !query.statistics.length) {
+      query.statistics = ['Average'];
+    }
+
+    if (!query.hasOwnProperty('matchExact')) {
+      query.matchExact = true;
+    }
+  }
 
   onChange(query: CloudWatchQuery) {
     const { onChange, onRunQuery } = this.props;
@@ -18,6 +70,116 @@ export class QueryEditor extends PureComponent<Props, State> {
   }
 
   render() {
-    return <QueryFieldsEditor {...this.props}></QueryFieldsEditor>;
+    const { data, query, onRunQuery } = this.props;
+    const { showMeta } = this.state;
+    const metaDataExist = data && Object.values(data).length && data.state === 'Done';
+    return (
+      <>
+        <QueryFieldsEditor {...this.props}></QueryFieldsEditor>
+        {query.statistics.length <= 1 && (
+          <div className="gf-form-inline">
+            <div className="gf-form">
+              <QueryField
+                className="query-keyword"
+                label="Id"
+                tooltip="Id can include numbers, letters, and underscore, and must start with a lowercase letter."
+              >
+                <Input
+                  className="gf-form-input width-8"
+                  onBlur={onRunQuery}
+                  onChange={(event: ChangeEvent<HTMLInputElement>) =>
+                    this.onChange({ ...query, id: event.target.value })
+                  }
+                  validationEvents={idValidationEvents}
+                  value={query.id || ''}
+                />
+              </QueryField>
+            </div>
+            <div className="gf-form gf-form--grow">
+              <QueryField
+                className="gf-form--grow"
+                label="Expression"
+                tooltip="Optionally you can add an expression here. Please note that if a math expression that is referencing other queries is being used, it will not be possible to create an alert rule based on this query"
+              >
+                <Input
+                  className="gf-form-input"
+                  onBlur={onRunQuery}
+                  value={query.expression || ''}
+                  onChange={(event: ChangeEvent<HTMLInputElement>) =>
+                    this.onChange({ ...query, expression: event.target.value })
+                  }
+                />
+              </QueryField>
+            </div>
+          </div>
+        )}
+        <div className="gf-form-inline">
+          <div className="gf-form">
+            <QueryField className="query-keyword" label="Period" tooltip="Minimum interval between points in seconds">
+              <Input
+                className="gf-form-input width-8"
+                value={query.period || ''}
+                placeholder="auto"
+                onBlur={onRunQuery}
+                onChange={(event: ChangeEvent<HTMLInputElement>) =>
+                  this.onChange({ ...query, period: event.target.value })
+                }
+              />
+            </QueryField>
+          </div>
+          <div className="gf-form">
+            <QueryField
+              className="query-keyword"
+              label="Alias"
+              tooltip="Alias replacement variables: {{metric}}, {{stat}}, {{namespace}}, {{region}}, {{period}}, {{label}}, {{YOUR_DIMENSION_NAME}}"
+            >
+              <Alias value={query.alias} onChange={(value: string) => this.onChange({ ...query, alias: value })} />
+            </QueryField>
+            <Switch
+              label="Match Exact"
+              labelClass="query-keyword"
+              tooltip="Only show metrics that exactly match all defined dimension names."
+              checked={query.matchExact}
+              onChange={() => this.onChange({ ...query, matchExact: !query.matchExact })}
+            />
+            <label className="gf-form-label">
+              <a
+                onClick={() =>
+                  metaDataExist &&
+                  this.setState({
+                    showMeta: !showMeta,
+                  })
+                }
+              >
+                <i className={`fa fa-caret-${showMeta ? 'down' : 'right'}`} /> {showMeta ? 'Hide' : 'Show'} Query
+                Preview
+              </a>
+            </label>
+          </div>
+          <div className="gf-form gf-form--grow">
+            <div className="gf-form-label gf-form-label--grow" />
+          </div>
+          {showMeta && metaDataExist && (
+            <table className="filter-table form-inline">
+              <thead>
+                <tr>
+                  <th>Metric Data Query ID</th>
+                  <th>Metric Data Query Expression</th>
+                  <th />
+                </tr>
+              </thead>
+              <tbody>
+                {data.series[0].meta.gmdMeta.map(({ ID, Expression }: any) => (
+                  <tr key={ID}>
+                    <td>{ID}</td>
+                    <td>{Expression}</td>
+                  </tr>
+                ))}
+              </tbody>
+            </table>
+          )}
+        </div>
+      </>
+    );
   }
 }
